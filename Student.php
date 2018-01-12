e01f3e8dba18bd1e7b08b9c9d9a45f34ecd41a8e
SamCollins <samcollins427@gmail.com>
2018-01-11 16:37:08 -0800

    Commented Student class

--- Student.php
+++ Student.php
@@ -8,11 +8,12 @@
 
 /**
  * Description of Student
- *
+ * Lab 1
  * @author Sam Collins
  */
 class Student {
     
+    //Creates a new Student object
     function __construct() {
         $this->surname = '';
         $this->first_name = '';    
@@ -21,14 +22,17 @@ class Student {
 
     }
     
+    //Adds an email to a student
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
     
+    //Adds a grade to a student
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    //Calculates the students average based on their grades
     function average() {
         $total = 0;
         foreach ($this->grades as $value) {
@@ -37,6 +41,7 @@ class Student {
         return $total / count($this->grades);
     }
     
+    //Displays all the info about a student as a string
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
